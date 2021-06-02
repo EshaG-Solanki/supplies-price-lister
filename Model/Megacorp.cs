9f58a8b6490102652b25c5c6e26064ee461955c5
Esha Solanki <gulyani.esha@gmail.com>
2021-06-02 17:05:41 +1000

    Added (empty) needed files

---
+++ Model/Megacorp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace buildxact_supplies.Model
+{
+    class Megacorp
+    {
+    }
+}
---
+++ Providers/MegacorpProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace buildxact_supplies.Providers
+{
+    class MegacorpProvider
+    {
+        //Fill data from json file
+    }
+}
